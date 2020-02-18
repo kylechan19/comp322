ed2a3f870228ebdf4a3704043f7766d095837c02
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-02-18 10:28:51 -0800

    Initial commit, included libraries + skeleton code.

---
+++ lab1/lab1.c
@@ -0,0 +1,35 @@
+/* Kyle Chan
+    Lab 1: Time and Forking Children
+    Prof. Benson
+    29 February, 2020 */
+
+#include <time.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void createProc()
+{
+    // Creates a process
+}
+
+void createChild()
+{
+    // Creates a child process
+}
+
+void reportProcInfo()
+{
+    // Used by both parent and child proccesses to print their own information.
+}
+
+int main(int argc, char **argv) 
+{
+
+    printf("End of lab1.c");
+    return 0;
+}
