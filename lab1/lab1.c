97c9c40fd29de34f7961c7ae4febe5a8e3b396ac
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-02-24 11:49:39 -0800

    Finish the project.

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -11,25 +11,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void createProc()
-{
-    // Creates a process
-}
-
-void createChild()
-{
-    // Creates a child process
-}
-
-void reportProcInfo()
+void reportProcInfo(int childStatus, int cpid)
 {
     // Used by both parent and child proccesses to print their own information.
+
+    // Print PID and PPID
+    printf("PID: %d PPID: %d ", 
+        getpid(),  
+        getppid());
+
+    // If applicable, print CPID and the child process' return value.
+    if (cpid > 0) {
+        printf("CPID: %d RETVAL: %d\n", 
+            cpid, 
+            childStatus);
+            
+    } 
+    else {
+        // format then close child process
+        printf("\n");
+        _exit(EXIT_SUCCESS);
+    }
+    
 }
 
-int main(int argc, char **argv) 
+int main() 
 {
+    // declare vars and times() struct
+    int sec, childStatus, cpid;
+    struct tms daTimes;
 
-    printf("End of lab1.c");
+    // Print START time
+    sec = time(NULL);
+    printf("START %d\n", sec);
+
+    childStatus = fork(); // Create child process and store its status
+
+    cpid = waitpid(childStatus, &childStatus, 0); // Wait for child to finish
+
+    reportProcInfo(childStatus, cpid); // Parent + child report on their information
+
+    times(&daTimes); // Store process times
+
+    // Print process times
+    printf("USER: %ld SYS: %ld\nCUSER: %ld CSYS: %ld\n",
+        (long)daTimes.tms_utime,
+        (long)daTimes.tms_stime,
+        (long)daTimes.tms_cutime,
+        (long)daTimes.tms_cstime);
+
+    // Print END time
+    sec = time(NULL);
+    printf("END %d\n", sec);
+    
     return 0;
 }
