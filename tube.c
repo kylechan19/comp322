9fadb7075037f3e6037f0fd7605a2d33d32bd875
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-03-06 19:09:11 -0800

    Small work on launch

--- tube.c
+++ tube.c
@@ -1,5 +1,5 @@
 /* Kyle Chan
-    Lab 2: Launch-Tube, launch.c
+    Lab 2: Launch-Tube, tube.c
     Prof. Benson
     17 March, 2020 */
 
@@ -9,23 +9,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void childsPlay(int argc, char *argv[], char *envp[]) 
-{
-    // Prepare new argv
-    char *argvChild[argc-2];
+void childsPlay(int argc, char *argv[], char *envp[]) {
 
-    // Load all args, except for the first two, into the new array
-    for (i = 2; i < argc; i++) {
-        argvChild[i] = argv[i];
-    }
-    // Child executes with supplied commands
-    execve(argv[1], argvChild, environ);
 }
 
 int main(int argc, char *argv[], char *envp[]) 
 {
     // Declare vars
-    int childStatus, cpid, i;
+    int childStatus, cpid;
 
     cpid = fork();
     if (cpid > 0) {
@@ -38,5 +29,6 @@ int main(int argc, char *argv[], char *envp[])
     } else {
         // err
     }
+
     return 0;
 }
